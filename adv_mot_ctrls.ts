03d6df9de74b3627f098ba3b1bec93bb515d21d5
Dmitriy Antipov <aston_bonny@mail.ru>
2024-02-21 21:43:51 +0300

    Обновить adv_mot_ctrls.ts, test.ts

--- adv_mot_ctrls.ts
+++ adv_mot_ctrls.ts
@@ -1,8 +1,8 @@
 /**
-* OFDL Advanced Motor Controller Block module (algorithm part).
+* Motor controllers based OFDL Advanced Motor Controller Block module (algorithm part).
 * Based 1.1 ver, 2023/09/27.
 */
-//% block="AdvMotCtrls"
+//% block="AdvMotCtrls" weight=89 color=#02ab38 icon="\uf3fd"
 namespace advmotctrls2 {
 
     let pwr: number;
@@ -12,7 +12,39 @@ namespace advmotctrls2 {
     let syncVLeftSign: number;
     let syncVRightSign: number;
 
+    /*
+    let ACC1_minPwr: number;
+    let ACC1_maxPwr: number;
+    let ACC1_accelDist: number;
+    let ACC1_decelDist: number;
+    let ACC1_totalDist: number;
+    let ACC1_isNEG: number;
+    */
+
+    let ACC2_minPwr: number;
+    let ACC2_maxPwr: number;
+    let ACC2_accelDist: number;
+    let ACC2_decelDist: number;
+    let ACC2_totalDist: number;
+    let ACC2_isNEG: number;
+
+    interface MotorsPower {
+        pwrLeft: number;
+        pwrRight: number;
+    }
+
+    interface AccTwoEncReturn {
+        pwrOut: number;
+        isDone: boolean;
+    }
+
+    /**
+        Конфигурация синхронизации моторов шассии.
+        @param vLeft входное значение скорости левого мотора, eg. 50
+        @param vRight входное значение скорости правого мотора, eg. 50
+    **/
     //% blockId=SyncMotorsConfig
+    //% block="Configuraton sync сhassis control vLeft = $vLeft|vRight = $vRight"
     export function SyncMotorsConfig(vLeft: number, vRight: number) {
         syncVLeft = vLeft;
         syncVRight = vRight;
@@ -20,23 +52,125 @@ namespace advmotctrls2 {
         syncVRightSign = Math.abs(vRight + 1) - Math.abs(vRight);
     }
 
+    /**
+        Посчитать ошибку синхронизации моторов шассии с использованием значений с энкодеров.
+        Возвращает число ошибки для регулятора.
+        @param eLeft входное значение энкодера левого мотора
+        @param eRight входное значение энкодера правого мотора
+    **/
     //% blockId=GetErrorSyncMotors
+    //% block="Get error sync chassis motors vLeft = $vLeft|vRight = $vRight"
     export function GetErrorSyncMotors(eLeft: number, eRight: number): number {
         return ((syncVRight * eLeft) - (syncVLeft * eRight));
     }
 
-    interface MotorsPower {
-        powerLeft: number;
-        powerRight: number;
-    }
-
-    //% blockId=GetPowerSyncMotors
-    export function GetPowerSyncMotors(U: number): MotorsPower {
+    
+    /**
+        Получить значения скоростей (мощности) для моторов шассии на основе управляющего воздействия, полученного от регулятора.
+        Возвращает интерфейс скорости (мощности) левого и правого моторов.
+        @param U входное значение управляющего воздействия от регулятора
+    **/
+    //% blockId=GetPwrSyncMotors
+    //% block="Configuraton sync сhassis control vLeft = $vLeft|vRight = $vRight"
+    export function GetPwrSyncMotors(U: number): MotorsPower {
         const pLeft = syncVLeft - syncVRightSign * U;
         const pRight = syncVRight + syncVLeftSign * U;
         return {
-            powerLeft: pLeft,
-            powerRight: pRight
+            pwrLeft: pLeft,
+            pwrRight: pRight
+        };
+    }
+    
+    /*
+    function SyncPwrIn(eLeft: number, eRight: number, vLeft: number, vRight: number) {
+        const U = ((vRight * eLeft) - (vLeft * eRight)) * syncKp;
+        const pLeft = vLeft - (Math.abs(vRight + 1) - Math.abs(vRight)) * U;
+        const pRight = vRight + (Math.abs(vLeft + 1) - Math.abs(vLeft)) * U;
+        leftMotor.run(pLeft);
+        rightMotor.run(pRight);
+    }
+    */
+
+    export function GetErrorSyncMotorsInPwr(eLeft: number, eRight: number, vLeft: number, vRight: number): number {
+        return ((vRight * eLeft) - (vLeft * eRight));
+    }
+
+    export function GetPwrSyncMotorsInPwr(U: number, vLeft: number, vRight: number) {
+        const pLeft = vLeft - (Math.abs(vRight + 1) - Math.abs(vRight)) * U;
+        const pRight = vRight + (Math.abs(vLeft + 1) - Math.abs(vLeft)) * U;
+        return {
+            pwrLeft: pLeft,
+            pwrRight: pRight
+        };
+    }
+
+    /**
+       Конфигурация ускорения и замедления шассии двух моторов.
+       @param minPwr входное значение скорости на старте, eg. 15
+       @param maxPwr входное значение максимальной скорости, eg. 50
+       @param accelDist значение дистанции ускорения, eg. 150
+       @param decelDist значение дистанции замедления, eg. 150
+       @param totalDist значение всей дистанции, eg. 500
+    **/
+    //% blockId=AccTwoEncConfig
+    //% block="Accel/deceleration configuration chassis control at minPwr = $minPwr|maxPwr = $maxPwr|accelDist = $accelDist|decelDist = $decelDist|totalDist = $totalDist"
+    export function AccTwoEncConfig(minPwr: number, maxPwr: number, accelDist: number, decelDist: number, totalDist: number) {
+        ACC2_minPwr = Math.abs(minPwr);
+        ACC2_maxPwr = Math.abs(maxPwr);
+        ACC2_accelDist = accelDist;
+        ACC2_decelDist = decelDist;
+        ACC2_totalDist = totalDist;
+        if (minPwr < 0) ACC2_isNEG = 1;
+        else ACC2_isNEG = 0;
+    }
+
+    /**
+       Конфигурация ускорения и замедления двумя моторами.
+       @param minPwr входное значение скорости на старте, eg. 15
+       @param maxPwr входное значение максимальной скорости, eg. 50
+       @param accelDist значение дистанции ускорения, eg. 150
+       @param decelDist значение дистанции замедления, eg. 150
+       @param totalDist значение всей дистанции, eg. 500
+    **/
+    //% blockId=AccTwoEnc
+    //% block="Accel/deceleration chassis control compute at encoder left = $eLeft|right = $eRight"
+    export function AccTwoEnc(eLeft: number, eRight: number): AccTwoEncReturn {
+        let done: boolean;
+        let pwrOut: number;
+        let currEnc = (Math.abs(eLeft) + Math.abs(eRight)) / 2;
+        if (currEnc >= ACC2_totalDist) {
+            done = true;
+        } else if (currEnc > ACC2_totalDist / 2) {
+            if (ACC2_decelDist == 0) {
+                pwr = ACC2_maxPwr;
+            } else {
+                pwr = (ACC2_maxPwr - ACC2_minPwr) / Math.pow(ACC2_decelDist, 2) * Math.pow(currEnc - ACC2_totalDist, 2) + ACC2_minPwr;
+            }
+            done = false;
+        } else {
+            if (ACC2_accelDist == 0) {
+                pwr = ACC2_maxPwr;
+            } else {
+                pwr = (ACC2_maxPwr - ACC2_minPwr) / Math.pow(ACC2_accelDist, 2) * Math.pow(currEnc - 0, 2) + ACC2_minPwr;
+            }
+            done = false;
+        }
+
+        if (pwr < ACC2_minPwr) {
+            pwr = ACC2_minPwr;
+        } else if (pwr > ACC2_maxPwr) {
+            pwr = ACC2_maxPwr;
+        }
+
+        if (ACC2_isNEG == 1) {
+            pwrOut = 0 - pwr;
+        } else {
+            pwrOut = pwr;
+        }
+
+        return {
+            pwrOut: pwrOut,
+            isDone: done
         };
     }
 
--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
-function MyExample51() {
+// Синхроннизированное движение на двух средних моторах на расстояние в 600 тиков энкодера
+function Example1() {
     advmotctrls2.SyncMotorsConfig(30, 30);
 
     automation.pid1.setGains(0.03, 0, 0.5); // Установка значений регулятору
@@ -12,19 +13,80 @@ function MyExample51() {
         let loopTime = currTime - prevTime;
         prevTime = currTime;
 
-        const encB = Math.abs(motors.mediumB.angle());
-        const encC = Math.abs(motors.mediumC.angle());
-        const error = advmotctrls2.GetErrorSyncMotors(encB, encC);
-        automation.pid1.setPoint(error);
-        const U = automation.pid1.compute(loopTime, 0);
-        const powers = advmotctrls2.GetPowerSyncMotors(U);
-        motors.mediumB.run(powers.powerLeft);
-        motors.mediumC.run(powers.powerRight);
-
+        let encB = motors.mediumB.angle();
+        let encC = motors.mediumC.angle();
         if ((encB + encC) / 2 >= 600) break;
+
+        let error = advmotctrls2.GetErrorSyncMotors(encB, encC);
+        automation.pid1.setPoint(error);
+        let U = automation.pid1.compute(loopTime, 0);
+        let powers = advmotctrls2.GetPwrSyncMotors(U);
+        motors.mediumB.run(powers.pwrLeft);
+        motors.mediumC.run(powers.pwrRight);
         control.timer1.pauseUntil(1);
     }
-    motors.mediumBC.stop();
+    motors.mediumBC.stop(); // Остановить моторы
+}
+
+function Example2() {
+    advmotctrls2.SyncMotorsConfig(25, 50);
+
+    automation.pid1.setGains(0.03, 0, 0.5); // Установка значений регулятору
+    automation.pid1.setControlSaturation(-100, 100); // Ограничения ПИДа
+    automation.pid1.reset(); // Сброс ПИДа
+
+    let prevTime = 0;
+    while (true) {
+        control.timer1.reset();
+        let currTime = control.millis()
+        let loopTime = currTime - prevTime;
+        prevTime = currTime;
+
+        let encB = motors.mediumB.angle();
+        let encC = motors.mediumC.angle();
+        if ((encB + encC) / 2 >= 775) break;
+
+        let error = advmotctrls2.GetErrorSyncMotors(encB, encC);
+        automation.pid1.setPoint(error);
+        let U = automation.pid1.compute(loopTime, 0);
+        let powers = advmotctrls2.GetPwrSyncMotors(U);
+        motors.mediumB.run(powers.pwrLeft);
+        motors.mediumC.run(powers.pwrRight);
+
+        control.timer1.pauseUntil(1);
+    }
+    motors.mediumBC.stop(); // Остановить моторы
+}
+
+function Example3() {
+    advmotctrls2.AccTwoEncConfig(15, 90, 100, 300, 1000);
+
+    automation.pid1.setGains(0.03, 0, 0.5); // Установка значений регулятору
+    automation.pid1.setControlSaturation(-100, 100); // Ограничения ПИДа
+    automation.pid1.reset(); // Сброс ПИДа
+    
+    let prevTime = 0;
+    while (true) {
+        control.timer1.reset();
+        let currTime = control.millis()
+        let loopTime = currTime - prevTime;
+        prevTime = currTime;
+
+        let encB = motors.mediumB.angle();
+        let encC = motors.mediumC.angle();
+        let out = advmotctrls2.AccTwoEnc(encB, encC);
+        if (out.isDone) break;
+
+        let error = advmotctrls2.GetErrorSyncMotorsInPwr(encB, encC, out.pwrOut, out.pwrOut);
+        automation.pid1.setPoint(error);
+        let U = automation.pid1.compute(loopTime, 0);
+        let powers = advmotctrls2.GetPwrSyncMotorsInPwr(U, out.pwrOut, out.pwrOut);
+        motors.mediumB.run(powers.pwrLeft);
+        motors.mediumC.run(powers.pwrRight);
+
+        control.timer1.pauseUntil(1);
+    }
+    motors.mediumBC.stop(); // Остановить моторы
 }
 
 // Функция для нормализации сырых значений с датчика
@@ -40,16 +102,17 @@ const W_REF_RAW_CS2 = 423;
 const B_REF_RAW_CS3 = 640;
 const W_REF_RAW_CS3 = 462;
 
-function Main() {
+function Test() {
     motors.mediumB.setInverted(true); motors.mediumC.setInverted(false);
+    motors.mediumB.setRegulated(false); motors.mediumC.setRegulated(false)
     motors.mediumB.setBrake(true); motors.mediumC.setBrake(true);
     motors.mediumB.stop(); motors.mediumC.stop();
     motors.mediumB.clearCounts(); motors.mediumC.clearCounts();
     brick.printString("RUN", 7, 13);
     brick.buttonEnter.pauseUntil(ButtonEvent.Pressed);
     brick.clearScreen();
-    MyExample51();
+    Example3();
     brick.buttonEnter.pauseUntil(ButtonEvent.Pressed);
 }
 
-Main();
+Test();
