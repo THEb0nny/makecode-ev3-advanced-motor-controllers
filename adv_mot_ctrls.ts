ce343a8e7240245e0c39c8687794fc66e5ff1c61
Dmitriy Antipov <aston_bonny@mail.ru>
2024-02-21 20:05:31 +0300

    Обновить adv_mot_ctrls.ts, test.ts

--- adv_mot_ctrls.ts
+++ adv_mot_ctrls.ts
@@ -1,1 +1,43 @@
-// Добавьте код здесь
+/**
+* OFDL Advanced Motor Controller Block module (algorithm part).
+* Based 1.1 ver, 2023/09/27.
+*/
+//% block="AdvMotCtrls"
+namespace advmotctrls2 {
+
+    let pwr: number;
+
+    let syncVLeft: number;
+    let syncVRight: number;
+    let syncVLeftSign: number;
+    let syncVRightSign: number;
+
+    //% blockId=SyncMotorsConfig
+    export function SyncMotorsConfig(vLeft: number, vRight: number) {
+        syncVLeft = vLeft;
+        syncVRight = vRight;
+        syncVLeftSign = Math.abs(vLeft + 1) - Math.abs(vLeft);
+        syncVRightSign = Math.abs(vRight + 1) - Math.abs(vRight);
+    }
+
+    //% blockId=GetErrorSyncMotors
+    export function GetErrorSyncMotors(eLeft: number, eRight: number): number {
+        return ((syncVRight * eLeft) - (syncVLeft * eRight));
+    }
+
+    interface MotorsPower {
+        powerLeft: number;
+        powerRight: number;
+    }
+
+    //% blockId=GetPowerSyncMotors
+    export function GetPowerSyncMotors(U: number): MotorsPower {
+        const pLeft = syncVLeft - syncVRightSign * U;
+        const pRight = syncVRight + syncVLeftSign * U;
+        return {
+            powerLeft: pLeft,
+            powerRight: pRight
+        };
+    }
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,55 @@
-// Добавьте код здесь
+function MyExample51() {
+    advmotctrls2.SyncMotorsConfig(30, 30);
+
+    automation.pid1.setGains(0.03, 0, 0.5); // Установка значений регулятору
+    automation.pid1.setControlSaturation(-100, 100); // Ограничения ПИДа
+    automation.pid1.reset(); // Сброс ПИДа
+
+    let prevTime = 0;
+    while (true) {
+        control.timer1.reset();
+        let currTime = control.millis()
+        let loopTime = currTime - prevTime;
+        prevTime = currTime;
+
+        const encB = Math.abs(motors.mediumB.angle());
+        const encC = Math.abs(motors.mediumC.angle());
+        const error = advmotctrls2.GetErrorSyncMotors(encB, encC);
+        automation.pid1.setPoint(error);
+        const U = automation.pid1.compute(loopTime, 0);
+        const powers = advmotctrls2.GetPowerSyncMotors(U);
+        motors.mediumB.run(powers.powerLeft);
+        motors.mediumC.run(powers.powerRight);
+
+        if ((encB + encC) / 2 >= 600) break;
+        control.timer1.pauseUntil(1);
+    }
+    motors.mediumBC.stop();
+}
+
+// Функция для нормализации сырых значений с датчика
+function GetNormRefValCS(refRawValCS: number, bRefRawValCS: number, wRefRawValCS: number): number {
+    let refValCS = Math.map(refRawValCS, bRefRawValCS, wRefRawValCS, 0, 100);
+    refValCS = Math.constrain(refValCS, 0, 100);
+    return refValCS;
+}
+
+const B_REF_RAW_CS2 = 652;
+const W_REF_RAW_CS2 = 423;
+
+const B_REF_RAW_CS3 = 640;
+const W_REF_RAW_CS3 = 462;
+
+function Main() {
+    motors.mediumB.setInverted(true); motors.mediumC.setInverted(false);
+    motors.mediumB.setBrake(true); motors.mediumC.setBrake(true);
+    motors.mediumB.stop(); motors.mediumC.stop();
+    motors.mediumB.clearCounts(); motors.mediumC.clearCounts();
+    brick.printString("RUN", 7, 13);
+    brick.buttonEnter.pauseUntil(ButtonEvent.Pressed);
+    brick.clearScreen();
+    MyExample51();
+    brick.buttonEnter.pauseUntil(ButtonEvent.Pressed);
+}
+
+Main();
