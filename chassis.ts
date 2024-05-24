002bacc6e624d8d4dd8362fb45405719cd0aad0d
Dmitriy Antipov <aston_bonny@mail.ru>
2024-05-24 11:00:52 +0300

    Обновить pxt.json, chassis.ts, enums.ts

--- chassis.ts
+++ chassis.ts
@@ -1,35 +1,3 @@
-const enum MeasurementUnit {
-    //% block="cm"
-    //% block.loc.ru="см"
-    Centimeters,
-    //% block="mm"
-    //% block.loc.ru="мм"
-    Millimeters
-}
-
-// Перечисление о типах относительных поворотов
-const enum WheelPivot {
-    //% block="left"
-    //% block.loc.ru="левого колеса"
-    LeftWheel,
-    //% block="right"
-    //% block.loc.ru="правого колеса"
-    RightWheel
-}
-
-// Перечислении о типах торможения
-const enum Braking {
-    //% block="break at hold"
-    //% block.loc.ru="торможение с удержанием"
-    Hold,
-    //% block="no break"
-    //% block.loc.ru="торможение без удержания"
-    NoBreak,
-    //% block="nothing"
-    //% block.loc.ru="не тормозить"
-    NoStop
-}
-
 /**
  * A differential drive robot.
  * Робот с дифференциальным приводом.
